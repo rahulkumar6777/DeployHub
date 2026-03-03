642870c68bb816e6f38f0c34259719a60b46780e
rahulkumar6777 <rahulk48546@gmail.com>
2026-03-04 01:55:46 +0530

    update

--- prototype/app-frontend/src/context/AuthContext.jsx
+++ prototype/app-frontend/src/context/AuthContext.jsx
@@ -7,7 +7,7 @@ import React, {
   useCallback,
 } from "react";
 import { useNavigate } from "react-router-dom";
-import { api, attachInterceptors } from "../api/apiClient";
+import { api, attachInterceptors } from "../api/apiclient";
 
 const AuthContext = createContext();
 
