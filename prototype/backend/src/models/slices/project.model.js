9f05fd19ed8f61913795f8be1a95f3a9d2f5522a
rahulkumar6777 <rahulk48546@gmail.com>
2026-03-05 00:37:54 +0530

    update

--- prototype/backend/src/models/slices/project.model.js
+++ prototype/backend/src/models/slices/project.model.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const projectSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        default: 'Unnamed Project'
     },
     owner: {
         type: mongoose.Schema.Types.ObjectId,
