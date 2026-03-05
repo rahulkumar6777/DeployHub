847c5003bc3b7bec9cc424ee68a2e115f9c8e747
rahulkumar6777 <rahulk48546@gmail.com>
2026-03-05 18:46:23 +0530

    update

--- prototype/backend/src/utils/sslUtils.js
+++ prototype/backend/src/utils/sslUtils.js
@@ -138,27 +138,24 @@ server {
         root /usr/share/nginx/html;
     }
     location / {
-        return 301 https://${domain}$request_uri;
+        return 301 https://${domain}\$request_uri;
     }
 }
-
 server {
     listen 443 ssl;
     server_name ${domain} www.${domain};
-
     ssl_certificate     ${LETSENCRYPT_DIR}/${domain}/fullchain.pem;
     ssl_certificate_key ${LETSENCRYPT_DIR}/${domain}/privkey.pem;
     ssl_protocols TLSv1.2 TLSv1.3;
     ssl_ciphers HIGH:!aNULL:!MD5;
-
     location / {
         proxy_pass ${PROXY_TARGET};
         proxy_http_version 1.1;
-        proxy_set_header Host              \\$host;
-        proxy_set_header X-Real-IP         \\$remote_addr;
-        proxy_set_header X-Forwarded-For   \\$proxy_add_x_forwarded_for;
+        proxy_set_header Host              \$host;
+        proxy_set_header X-Real-IP         \$remote_addr;
+        proxy_set_header X-Forwarded-For   \$proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto https;
-        proxy_set_header Upgrade           \\$http_upgrade;
+        proxy_set_header Upgrade           \$http_upgrade;
         proxy_set_header Connection        "upgrade";
         client_max_body_size 200M;
     }
