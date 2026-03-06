51cc184e0b4802565fadfaa554292fd6098d5a3d
rahulkumar6777 <rahulk48546@gmail.com>
2026-03-06 15:05:55 +0530

    structure controller

--- Refactor/Prototype/Backend/src/controllers/index.js
+++ Refactor/Prototype/Backend/src/controllers/index.js
@@ -1,11 +1,19 @@
 import { initRegistration } from "./Auth/initRegister.controller";
+import { Login } from "./Auth/Login.controller";
+import { Logout } from "./Auth/Logout.Controller";
+import { RefreshToken } from "./Auth/RefreshToken.Controller";
 import { verifyRegister } from "./Auth/verifyRegister.controller";
 
 export const controller = {
     Auth: {
-        Register: {
-            init: initRegistration,
-            verify: verifyRegister
-        }
+        Local: {
+            Register: {
+                init: initRegistration,
+                verify: verifyRegister
+            },
+            Login,
+        },
+        RefreshToken,
+        Logout
     }
 }
