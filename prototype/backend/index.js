757ab99e15358e5e0026be5effd3de095c78a0b6
Rahul Kumar <161327448+rahulkumar6777@users.noreply.github.com>
2026-03-18 00:14:10 +0530

    Update index.js

--- prototype/backend/index.js
+++ prototype/backend/index.js
@@ -40,7 +40,7 @@ app.use(helmet());
 import cookieParser from "cookie-parser";
 app.use(cookieParser());
 
-app.use('/health' , (req , res)=> {
+app.use('/' , (req , res)=> {
     res.send("service Online")
 })
 export default app;
