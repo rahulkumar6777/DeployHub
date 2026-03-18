71a1461937cf8a6188c7fbe1f8bdc07581717214
rahulkumar6777 <rahulk48546@gmail.com>
2026-03-18 22:14:35 +0530

    update

--- prototype/backend/index.js
+++ prototype/backend/index.js
@@ -39,8 +39,4 @@ app.use(helmet());
 
 import cookieParser from "cookie-parser";
 app.use(cookieParser());
-
-app.use('/health' , (req , res)=> {
-    res.send("service Online")
-})
 export default app;
